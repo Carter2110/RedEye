63f7502a74fa2585747931995d99ac95993c2b59
Carter Kwon <ckwon6@sundevils.asu.edu>
2017-06-04 02:53:11 -0700

    tried adding output functionality

--- RedEye/Form2.Designer.cs
+++ RedEye/Form2.Designer.cs
@@ -36,6 +36,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.serialPorty = new System.IO.Ports.SerialPort(this.components);
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtInput
@@ -92,11 +93,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(428, 305);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 61);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(666, 423);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -105,6 +117,7 @@
             this.Controls.Add(this.txtInput);
             this.Name = "Form2";
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,5 +132,6 @@
         private System.Windows.Forms.Button button1;
         private System.IO.Ports.SerialPort serialPorty;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- RedEye/Form2.cs
+++ RedEye/Form2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ namespace RedEye
 {
     public partial class Form2 : Form
     {
+        static string output;
         public Form2()
         {
             InitializeComponent();
@@ -29,12 +31,32 @@ namespace RedEye
         private void button1_Click(object sender, EventArgs e)
         {
             serialPorty.Write(txtInput.Text);
+           
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             _parentForm.Close();
             Close();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            serialPorty.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+        private static void DataReceivedHandler( object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            output = indata;
+            //Console.WriteLine("Data Received:");
+            //Console.Write(indata);
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = output;
         }
     }
 }
